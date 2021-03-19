66b917688bb1459095fe176637aaabb1f1826a59
jodene garvey <jodenepatrice@hotmail.com>
2021-03-19 12:01:06 -0500

    CRUD Scaffolding - Jodene

--- EC2_NBC/Startup.cs
+++ EC2_NBC/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using EC2_NBC.Data;
 
 namespace EC2_NBC
 {
@@ -24,6 +26,9 @@ namespace EC2_NBC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<EC2_NBCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EC2_NBCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ EC2_NBC/ViewModels/CreateCustomer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EC2_NBC.ViewModels
+{
+    public class CreateCustomer
+    {
+
+        [Required(ErrorMessage = "Enter Name")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Address"), MaxLength(40)]
+        [DataType(DataType.Text)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter Balance of $5000 or more")]
+        [Range(5000, 10000, ErrorMessage = "Balance must be between $5000 and $10000")]
+        [DataType(DataType.Currency)]
+        public float Balance { get; set; }
+
+        [Required(ErrorMessage = "Enter Account Number")]
+        [RegularExpression(@"^[0-9]{7}", ErrorMessage = "Enter 7 digit number")]
+        [DataType(DataType.CreditCard)]
+        public int AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Enter Account Number")]
+        [DataType(DataType.CreditCard)]
+        [RegularExpression(@"^[9505]+[0-9]{12}", ErrorMessage = "Enter a 12 digit card number starting with 9505")]
+        public int CardNumber { get; set; }
+
+        public string AccountType { get; set; }
+    }
+}
